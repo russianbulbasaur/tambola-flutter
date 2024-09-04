72e0f95bdd5b766c9c37bcd46ccb4ab92c4bd8ee
Bulbasaur <bulbasaur@localhost.localdomain>
2024-09-04 22:58:40 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tambola/screens/welcome_screen.dart';
+
+void main() {
+  runApp(const TambolaApp());
+}
+
+class TambolaApp extends StatelessWidget {
+  const TambolaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(designSize: const Size(930,640),
+    builder: (_,child){
+      return MaterialApp(
+        home: child,
+      );
+    },
+    child: const WelcomeScreen(),);
+  }
+}
+
